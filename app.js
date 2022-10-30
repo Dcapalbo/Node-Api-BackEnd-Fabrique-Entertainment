0b7d096420c3858298b3058bac2b7bfb75b39616
Domenico Capalbo <capalbodomenico@gmail.com>
2022-10-30 11:20:51 +0100

    scaffHolding

---
+++ app.js
@@ -0,0 +1,147 @@
+const path = require('path'); 
+const fs = require('fs'); 
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const session = require('express-session'); 
+const MongoDBStore = require('connect-mongodb-session')(session); 
+const csrf = require('csurf');
+const flash = require('connect-flash'); 
+const multer = require('multer');
+const helmet = require('helmet'); 
+const compression = require('compression'); 
+const morgan = require('morgan'); 
+const errorController = require('./controllers/error'); 
+const User = require('./models/user');
+
+// Take the mongodb Uri 
+const MONGODB_URI =
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@my-testing-cluster.nmev1.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`;
+
+// inizialize express 
+const app = express();
+// Store the sessions collection
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+
+// Inizialize csrf protection
+const csrfProtection = csrf();
+
+// Building filreStorage for images 
+// diskStorage implementation
+const fileStorage = multer.diskStorage({
+  // destination 
+  destination: (req, file, cb) => {
+    // callback, first argument the error the second is the destinations 
+    cb(null, 'images');
+  },
+  // filename 
+  filename: (req, file, cb) => {
+    // callback, first argument the error the second is the filename
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+});
+// make the filter variable 
+const fileFilter = (req, file, cb) => {
+  // if the format are this kind 
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    // insert it inside the storage 
+    cb(null, true);
+  } else {
+    // don't insert it 
+    cb(null, false);
+  }
+};
+
+// Importing the routes 
+
+
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, 'access.log'),
+  {
+    flags: 'a'
+  }
+)
+// inizialize helmet 
+app.use(helmet());
+// inizialize compression
+app.use(compression());
+// inizialize morgan
+app.use(morgan('combined', {
+  stream: accessLogStream
+})); 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// Inizialize multer 
+app.use(
+  // returning the storage and the filefilter 
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+
+// Inizialize csrf and flash 
+app.use(csrfProtection);
+app.use(flash());
+
+// Getting response for the authentication and the csrf token 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then(user => {
+      if (!user) {
+        return next();
+      }
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      next(new Error(err));
+    });
+});
+
+// Inizialise the routes 
+
+app.get('/500', errorController.get500);
+
+app.use(errorController.get404);
+
+app.use((error, req, res, next) => {
+  res.status(500).render('500', {
+    pageTitle: 'Error!',
+    path: '/500',
+    isAuthenticated: req.session.isLoggedIn
+  });
+});
+
+// Inizialise the server 
+mongoose
+  .connect(MONGODB_URI)
+  .then(result => {
+    app.listen(process.env.PORT || 5000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ model/film.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+// creating the Mongoose db Schema
+const Schema = mongoose.Schema;
+// products Schema 
+const filmSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+//   userId: {
+//      // grabbing the id using mongoose
+//     type: Schema.Types.ObjectId,
+//     // junction with ref, the Product and the User
+//     ref: 'User',
+//     required: true
+//   }
+});
+
+// / exporting the model and the Schema
+module.exports = mongoose.model('Film', filmSchema);
